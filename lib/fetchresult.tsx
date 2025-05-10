3647610d702048909c6b378a95d330133c7e0383
FEUSSI-NDE-PATERSON <peussipeuss@gmail.com>
2025-05-10 18:24:45 +0100

    First commit

--- lib/fetchresult.tsx
+++ lib/fetchresult.tsx
@@ -4,21 +4,7 @@ async function fetchresult(searchParams:SearchParams) {
     const userName = process.env.OXYLABS_USERNAME
     const password = process.env.OXYLABS_PASSWORD
     console.log(searchParams+"jpg")
-    if (searchParams.url) {
-        const url = new URL(searchParams.url);
-        // rest of your code
-        alert("fdgdfgf")
-      } else {
-        console.log('searchParams.url is empty or null');
-      }
-    Object.keys(searchParams).forEach((key)=>{
-        if(key === "url"|| key === "location") return ;
-        const value = searchParams[key as keyof SearchParams]
-        if(typeof value === "string"){
-            url.searchParams.append(key,value)
-            console.log(url+"scabbler")
-        }
-    })
+  
   return (
     <div>fetchresult</div>
   )
